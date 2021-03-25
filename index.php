c40872e2afdd233b67e996ea04b7e5f9b89ab68d
romanbatavi <romanbatavi98@gmail.com>
2021-03-25 20:12:34 +0700

    indexing

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+    include_once("function/helper.php");
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Roman Shop</title>
+    </head>
+
+    <body>
+        <div id='container'>
+            <div id='header'>
+                <a href="<?php echo BASE_URL."index.php"; ?>">
+                    <img src="<?php echo BASE_URL."images/logo2.png"; ?>"/>
+                </a>
+            
+            <div id="menu">
+                <div id="user">
+                    <a href="<?php echo BASE_URL."index.php?page=login"; ?>">Login</a>
+                    <a href="<?php echo BASE_URL."index.php?page=register"; ?>">Register</a>
+                </div>
+                <a href="<?php echo BASE_URL."index.php?page=keranjang"; ?>">
+                    <img src="<?php echo BASE_URL."images/cart.png"; ?>"/>
+                </a>
+            </div>
+            </div>
+
+            <div id="content">
+
+            </div>
+
+            <div id="footer">
+                <p>© Roman Shop : Roman Batavi 2018</p>
+            </div>
+        </div>
+    </body>
+</html>
